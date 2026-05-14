0cd5ad0df42cfb53fbac058479da4a13340a6af6
ly11133 <1375976002@qq.com>
2026-05-14 16:37:17 +0800

    Export public SD-CDM APIs

--- src/sd_cdm/__init__.py
+++ src/sd_cdm/__init__.py
@@ -1,5 +1,20 @@
 """SD-CDM package."""
 
-from .model import SDCDM, SDCDMTrainer
+from .data import DataBundle, InteractionDataset, build_data_bundle
+from .evaluate import binary_metrics, degree_of_agreement
+from .model import SDCDM, SDCDMTrainer, TrainerConfig
+from .utils import resolve_device, save_json, set_seed
 
-__all__ = ["SDCDM", "SDCDMTrainer"]
+__all__ = [
+    "DataBundle",
+    "InteractionDataset",
+    "SDCDM",
+    "SDCDMTrainer",
+    "TrainerConfig",
+    "binary_metrics",
+    "build_data_bundle",
+    "degree_of_agreement",
+    "resolve_device",
+    "save_json",
+    "set_seed",
+]
